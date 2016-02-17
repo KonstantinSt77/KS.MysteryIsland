a632d4a62439dd1c3c4e33e9bf3f47e60794c7ef
SergeyZakutniy <sergei.zakutnii@gmail.com>
2016-02-18 01:00:18 +0200

    commit version 1.0 Debug

---
+++ Debug/game_shakal/Tfield.cpp
@@ -0,0 +1,619 @@
+//#include "stdafx.h"
+//
+//using namespace field;
+//using namespace std;
+//
+//void Tfield::changeTurn()
+//{
+//	this->turnOfFirst = !this->turnOfFirst;
+//}
+//bool Tfield::getTurnOfFirst()
+//{
+//	return this->turnOfFirst;
+//}
+//Tfield::Tfield()
+//{
+//	srand(time(NULL));
+//	this->coins = 35;
+//	this->n = 12;
+//	this->turnOfFirst = true;
+//	this->fieldConst = new int*[n];
+//	this->fieldVisible = new int*[n];
+//	for (int i=0; i<n; i++)
+//	{
+//		this->fieldConst[i] = new int[n];
+//		this->fieldVisible[i] = new int[n];
+//	}
+//	for (int i=0; i<n; i++)
+//		for (int j=0; j<n; j++)
+//			this->fieldVisible[i][j] = field::unvisible;
+//	for (int i=0; i<n; i++)
+//	{
+//		this->fieldConst[0][i] = field::water;
+//		this->fieldConst[i][0] = field::water;
+//		this->fieldConst[i][n-1] = field::water;
+//		this->fieldConst[n-1][i] = field::water;
+//	}
+//	this->fieldConst[0][this->n/2] = field::shipBlue;
+//	this->fieldConst[this->n-1][this->n/2] = field::shipRed;
+//	//reserved place for ship landing
+//	this->fieldConst[1][this->n/2] = field::water;
+//	this->fieldConst[this->n-2][this->n/2] = field::water;
+//	for (int i = 1; i< n-1; i++)
+//		for (int j = 1; j< n-1; j++)
+//			this->fieldConst[i][j] = field::empty;
+//	for (int i=0; i<n; i++)
+//	{
+//		this->fieldVisible[0][i] =this->fieldConst[0][i];
+//		this->fieldVisible[i][0] = this->fieldConst[i][0];
+//		this->fieldVisible[i][n-1] = this->fieldConst[i][n-1];
+//		this->fieldVisible[n-1][i] = this->fieldConst[n-1][i];
+//	}
+//	//generation burned ground and lava
+//	int lavaR = rand()%(n-4)+2;
+//	int lavaM = rand()%(n-4)+2;
+//
+//	for (int i=lavaR-1; i<=lavaR+1; i++)
+//		for (int j=lavaM-1; j<=lavaM+1; j++)
+//			this->fieldConst[i][j] = field::burned;
+//
+//	this->fieldConst[lavaR][lavaM] = field::lava;
+//	//generation burned ground and lava
+//	int lavaX = rand()%(n-4)+2;
+//	int lavaY = rand()%(n-4)+2;
+//
+//	for (int i=lavaX-1; i<=lavaX+1; i++)
+//		for (int j=lavaY-1; j<=lavaY+1; j++)
+//			this->fieldConst[i][j] = field::burned;
+//
+//	this->fieldConst[lavaX][lavaY] = field::lava;
+//	//generation swamp
+//	int numberOfSwamps = 2;
+//	while(numberOfSwamps>0)
+//	{
+//		int swampX = rand()%(n-2)+1;
+//		int swampY = rand()%(n-2)+1;
+//		if (this->fieldConst[swampX][swampY]==field::empty)
+//		{
+//			this->fieldConst[swampX][swampY] = field::swamp;
+//			numberOfSwamps--;
+//		}
+//	}
+//	//generation coin1
+//	int numberOfCoins1 = 5;
+//	while(numberOfCoins1>0)
+//	{
+//		int coinX = rand()%(n-2)+1;
+//		int coinY = rand()%(n-2)+1;
+//		if (this->fieldConst[coinX][coinY]==field::empty)
+//		{
+//			this->fieldConst[coinX][coinY] = field::coin1;
+//			numberOfCoins1--;
+//		}
+//	}
+//	//generation coin2
+//	int numberOfCoins2 = 4;
+//	while(numberOfCoins2>0)
+//	{
+//		int coinX = rand()%(n-2)+1;
+//		int coinY = rand()%(n-2)+1;
+//		if (this->fieldConst[coinX][coinY]==field::empty)
+//		{
+//			this->fieldConst[coinX][coinY] = field::coin2;
+//			numberOfCoins2--;
+//		}
+//	}
+//	//generation coin3
+//	int numberOfCoins3 = 3;
+//	while(numberOfCoins3>0)
+//	{
+//		int coinX = rand()%(n-2)+1;
+//		int coinY = rand()%(n-2)+1;
+//		if (this->fieldConst[coinX][coinY]==field::empty)
+//		{
+//			this->fieldConst[coinX][coinY] = field::coin3;
+//			numberOfCoins3--;
+//		}
+//	}
+//	//generation coin4
+//	int numberOfCoins4 = 2;
+//	while(numberOfCoins4>0)
+//	{
+//		int coinX = rand()%(n-2)+1;
+//		int coinY = rand()%(n-2)+1;
+//		if (this->fieldConst[coinX][coinY]==field::empty)
+//		{
+//			this->fieldConst[coinX][coinY] = field::coin4;
+//			numberOfCoins4--;
+//		}
+//	}
+//    //generation coin5
+//	int numberOfCoins5 = 1;
+//	while(numberOfCoins5>0)
+//	{
+//		int coinX = rand()%(n-2)+1;
+//		int coinY = rand()%(n-2)+1;
+//		if (this->fieldConst[coinX][coinY]==field::empty)
+//		{
+//			this->fieldConst[coinX][coinY] = field::coin5;
+//			numberOfCoins5--;
+//		}
+//	}
+//	//generation ogre
+//	int numberOfogre = 2;
+//	while(numberOfogre>0)
+//	{
+//		int ogreX = rand()%(n-2)+1;
+//		int ogreY = rand()%(n-2)+1;
+//		if (this->fieldConst[ogreX][ogreY]==field::empty)
+//		{
+//			this->fieldConst[ogreX][ogreY] = field::ogre;
+//			numberOfogre--;
+//		}
+//	}	
+//	//generation shotgun
+//	int numberOfshotgun = 3;
+//	while(numberOfshotgun>0)
+//	{
+//		int shotgunX = rand()%(n-2)+1;
+//		int shotgunY = rand()%(n-2)+1;
+//		if (this->fieldConst[shotgunX][shotgunY]==field::empty)
+//		{
+//			this->fieldConst[shotgunX][shotgunY] = field::shotgun;
+//			numberOfshotgun--;
+//		}
+//	}	
+//	//generation eagle
+//	int numberOfeagle = 1;
+//	while(numberOfeagle>0)
+//	{
+//		int eagleX = rand()%(n-2)+1;
+//		int eagleY = rand()%(n-2)+1;
+//		if (this->fieldConst[eagleX][eagleY]==field::empty)
+//		{
+//			this->fieldConst[eagleX][eagleY] = field::eagle;
+//			numberOfeagle--;
+//		}
+//	}	
+//	//generation rope
+//	int numberOfrope = 4;
+//	while(numberOfrope>0)
+//	{
+//		int ropeX = rand()%(n-2)+1;
+//		int ropeY = rand()%(n-2)+1;
+//		if (this->fieldConst[ropeX][ropeY]==field::empty)
+//		{
+//			this->fieldConst[ropeX][ropeY] = field::rope;
+//			numberOfrope--;
+//		}
+//	}		
+//	//empty cells for ship landing
+//	this->fieldConst[1][this->n/2] = field::empty;
+//	this->fieldConst[this->n-2][this->n/2] = field::empty;
+//}
+////builder
+//Tfield::~Tfield()
+//{
+//	for (int i=0; i<n; i++)
+//	{
+//		delete[]this->fieldConst[i];
+//		delete[]this->fieldVisible[i];
+//	}
+//	delete[]this->fieldConst;
+//	delete[]this->fieldVisible;
+//}
+//int** Tfield::getFieldVisible()
+//{
+//	return this->fieldVisible;
+//}
+//int** Tfield::getFieldConst()
+//{
+//	return this->fieldConst;
+//}
+//void Tfield::doVisible(int x_,int y_)
+//{
+//	if (x_<n && y_<n)
+//	{
+//		fieldVisible[y_][x_] = fieldConst[y_][x_];
+//	}
+//}
+//int Tfield::getFieldPlace(int i, int j)
+//{
+//	return this->fieldConst[i][j];
+//}
+//int Tfield::getCoins()
+//{
+//	return this->coins;
+//}
+//void Tfield::changeField(int i_, int j_, int type_)
+//{
+//	switch (type_)
+//	{
+//	case field::ogre:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			break;
+//		}
+//	case field::shotgun:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			break;
+//		}
+//	case field::eagle:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			this->coins--;
+//			break;
+//		}
+//case field::coin1:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			this->coins--;
+//			break;
+//		}
+//		case field::coin2:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			this->coins--;
+//			break;
+//		}
+//		case field::coin3:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			this->coins--;
+//			break;
+//		}
+//		case field::coin4:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			this->coins--;
+//			break;
+//		}
+//		case field::coin5:
+//		{
+//			this->fieldConst[i_][j_] = field::empty;
+//			this->coins--;
+//			break;
+//		}
+//	}
+//}
+
+#include "stdafx.h"
+
+using namespace field;
+using namespace std;
+
+
+void Tfield::changeTurn()
+{
+	this->turnOfFirst = !this->turnOfFirst;
+}
+
+bool Tfield::getTurnOfFirst()
+{
+	return this->turnOfFirst;
+}
+
+Tfield::Tfield()
+{
+	srand(time(NULL));
+	this->coins = 35;
+	this->n = 12;
+
+	this->turnOfFirst = true;
+
+	this->fieldConst = new int*[n];
+	this->fieldVisible = new int*[n];
+
+	for (int i=0; i<n; i++)
+	{
+		this->fieldConst[i] = new int[n];
+		this->fieldVisible[i] = new int[n];
+	}
+
+	for (int i=0; i<n; i++)
+		for (int j=0; j<n; j++)
+			this->fieldVisible[i][j] = field::unvisible;
+
+	
+
+	for (int i=0; i<n; i++)
+	{
+		this->fieldConst[0][i] = field::water;
+		this->fieldConst[i][0] = field::water;
+		this->fieldConst[i][n-1] = field::water;
+		this->fieldConst[n-1][i] = field::water;
+
+	}
+
+	this->fieldConst[0][this->n/2] = field::shipBlue;
+	this->fieldConst[this->n-1][this->n/2] = field::shipRed;
+
+	//резервирование места для высадки с корабля
+	this->fieldConst[1][this->n/2] = field::water;
+	this->fieldConst[this->n-2][this->n/2] = field::water;
+
+	
+	for (int i = 1; i< n-1; i++)
+		for (int j = 1; j< n-1; j++)
+			this->fieldConst[i][j] = field::empty;
+
+
+	for (int i=0; i<n; i++)
+	{
+		this->fieldVisible[0][i] =this->fieldConst[0][i];
+		this->fieldVisible[i][0] = this->fieldConst[i][0];
+		this->fieldVisible[i][n-1] = this->fieldConst[i][n-1];
+		this->fieldVisible[n-1][i] = this->fieldConst[n-1][i];
+	}
+
+	//генерация лавы1
+	int lavaR = rand()%(n-4)+2;
+	int lavaM = rand()%(n-4)+2;
+
+	for (int i=lavaR-1; i<=lavaR+1; i++)
+		for (int j=lavaM-1; j<=lavaM+1; j++)
+			this->fieldConst[i][j] = field::burned;
+
+	this->fieldConst[lavaR][lavaM] = field::lava;
+	
+	//генерация лавы1
+	int lavaX = rand()%(n-4)+2;
+	int lavaY = rand()%(n-4)+2;
+
+	for (int i=lavaX-1; i<=lavaX+1; i++)
+		for (int j=lavaY-1; j<=lavaY+1; j++)
+			this->fieldConst[i][j] = field::burned;
+
+	this->fieldConst[lavaX][lavaY] = field::lava;
+	
+	
+	//генерация болота
+
+	int numberOfSwamps = 1;
+
+	while(numberOfSwamps>0)
+	{
+		int swampX = rand()%(n-2)+1;
+		int swampY = rand()%(n-2)+1;
+		if (this->fieldConst[swampX][swampY]==field::empty)
+		{
+			this->fieldConst[swampX][swampY] = field::swamp;
+			numberOfSwamps--;
+		}
+	}
+
+	//генерация монеты 1
+
+	int numberOfCoins1 = 3;
+
+	while(numberOfCoins1>0)
+	{
+		int coinX = rand()%(n-2)+1;
+		int coinY = rand()%(n-2)+1;
+		if (this->fieldConst[coinX][coinY]==field::empty)
+		{
+			this->fieldConst[coinX][coinY] = field::coin1;
+			numberOfCoins1--;
+		}
+	}
+
+	//генерация монеты 2
+
+	int numberOfCoins2 = 3;
+
+	while(numberOfCoins2>0)
+	{
+		int coinX = rand()%(n-2)+1;
+		int coinY = rand()%(n-2)+1;
+		if (this->fieldConst[coinX][coinY]==field::empty)
+		{
+			this->fieldConst[coinX][coinY] = field::coin2;
+			numberOfCoins2--;
+		}
+	}
+	//генерация монеты 3
+
+	int numberOfCoins3 = 2;
+
+	while(numberOfCoins3>0)
+	{
+		int coinX = rand()%(n-2)+1;
+		int coinY = rand()%(n-2)+1;
+		if (this->fieldConst[coinX][coinY]==field::empty)
+		{
+			this->fieldConst[coinX][coinY] = field::coin3;
+			numberOfCoins3--;
+		}
+	}
+	//генерация монеты 4
+
+	int numberOfCoins4 = 2;
+
+	while(numberOfCoins4>0)
+	{
+		int coinX = rand()%(n-2)+1;
+		int coinY = rand()%(n-2)+1;
+		if (this->fieldConst[coinX][coinY]==field::empty)
+		{
+			this->fieldConst[coinX][coinY] = field::coin4;
+			numberOfCoins4--;
+		}
+	}
+
+//	генерация монеты 5
+
+	int numberOfCoins5 = 1;
+
+	while(numberOfCoins5>0)
+	{
+		int coinX = rand()%(n-2)+1;
+		int coinY = rand()%(n-2)+1;
+		if (this->fieldConst[coinX][coinY]==field::empty)
+		{
+			this->fieldConst[coinX][coinY] = field::coin5;
+			numberOfCoins5--;
+		}
+	}
+
+
+//генерация людоеда
+	int numberOfogre = 2;
+
+	while(numberOfogre>0)
+	{
+		int ogreX = rand()%(n-2)+1;
+		int ogreY = rand()%(n-2)+1;
+		if (this->fieldConst[ogreX][ogreY]==field::empty)
+		{
+			this->fieldConst[ogreX][ogreY] = field::ogre;
+			numberOfogre--;
+		}
+	}	
+
+//генерация дробаш
+	int numberOfshotgun = 3;
+
+	while(numberOfshotgun>0)
+	{
+		int shotgunX = rand()%(n-2)+1;
+		int shotgunY = rand()%(n-2)+1;
+		if (this->fieldConst[shotgunX][shotgunY]==field::empty)
+		{
+			this->fieldConst[shotgunX][shotgunY] = field::shotgun;
+			numberOfshotgun--;
+		}
+	}	
+
+//генерация Орла Р.П.
+	int numberOfeagle = 8;
+
+	while(numberOfeagle>0)
+	{
+		int eagleX = rand()%(n-2)+1;
+		int eagleY = rand()%(n-2)+1;
+		if (this->fieldConst[eagleX][eagleY]==field::empty)
+		{
+			this->fieldConst[eagleX][eagleY] = field::eagle;
+			numberOfeagle--;
+		}
+	}
+
+		
+////генерация ВЕРЕВКА
+	int numberOfrope = 4;
+
+	while(numberOfrope>0)
+	{
+		int ropeX = rand()%(n-2)+1;
+		int ropeY = rand()%(n-2)+1;
+		if (this->fieldConst[ropeX][ropeY]==field::empty)
+		{
+			this->fieldConst[ropeX][ropeY] = field::rope;
+			numberOfrope--;
+		}
+	}		
+
+//пустые клетки для высадки с корабля
+	this->fieldConst[1][this->n/2] = field::empty;
+	this->fieldConst[this->n-2][this->n/2] = field::empty;
+
+
+
+}//конструктор
+
+
+Tfield::~Tfield()
+{
+	for (int i=0; i<n; i++)
+	{
+		delete[]this->fieldConst[i];
+		delete[]this->fieldVisible[i];
+	}
+	delete[]this->fieldConst;
+	delete[]this->fieldVisible;
+}
+
+int** Tfield::getFieldVisible()
+{
+	return this->fieldVisible;
+}
+
+int** Tfield::getFieldConst()
+{
+	return this->fieldConst;
+}
+
+void Tfield::doVisible(int x_,int y_)
+{
+	if (x_<n && y_<n)
+	{
+		fieldVisible[y_][x_] = fieldConst[y_][x_];
+	}
+
+}
+
+int Tfield::getFieldPlace(int i, int j)
+{
+	return this->fieldConst[i][j];
+}
+
+int Tfield::getCoins()
+{
+	return this->coins;
+}
+
+
+void Tfield::changeField(int i_, int j_, int type_)
+{
+	switch (type_)
+	{
+	case field::ogre:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			break;
+		}
+	case field::shotgun:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			break;
+		}
+	case field::eagle:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			this->coins--;
+			break;
+		}
+case field::coin1:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			this->coins--;
+			break;
+		}
+		case field::coin2:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			this->coins--;
+			break;
+		}
+		case field::coin3:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			this->coins--;
+			break;
+		}
+		case field::coin4:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			this->coins--;
+			break;
+		}
+		case field::coin5:
+		{
+			this->fieldConst[i_][j_] = field::empty;
+			this->coins--;
+			break;
+		}
+
+	}
+}
---
+++ Debug/game_shakal/Tplayer.cpp
@@ -0,0 +1,259 @@
+
+#include "stdafx.h"
+
+namespace player
+{
+
+	int myAbs(int n)
+	{
+		return (n>0)?n:-1*n;
+	}
+
+	int Tplayer::getCharacterI(int n_)
+	{
+		return this->arrayChar[n_].i;
+	}
+
+	int Tplayer::getCharacterJ(int n_)
+	{
+		return this->arrayChar[n_].j;
+	}
+
+	void Tplayer::setCharacActive(int c_)
+	{
+		this->characActive = c_;
+	}
+
+	bool Tplayer::moveCharacter(int i_, int j_, int fieldPlace_)
+	{
+		if (i_>=0 && i_<=12 && j_>=0 && j_<=12)
+		{
+			if (i_>= this->arrayChar[this->characActive].i-1 && i_<=this->arrayChar[this->characActive].i+1 &&
+				j_>= this->arrayChar[this->characActive].j-1 && j_<=this->arrayChar[this->characActive].j+1)
+			{
+				this->arrayChar[this->characActive].i = i_;
+				this->arrayChar[this->characActive].j = j_;
+
+				switch (fieldPlace_ )
+				{
+				case field::lava:
+					{
+						this->arrayChar[this->characActive].isLive = false;
+						break;
+					}
+				case field::water:
+                   {
+                        this->arrayChar[this->characActive].isLive = false;
+                        break;
+                   }
+				case field::ogre:
+					{
+						if (!this->arrayChar[this->characActive].hasShotgun)
+						{
+							this->arrayChar[this->characActive].isLive = false;
+						}
+						else
+						{
+							this->arrayChar[this->characActive].hasShotgun = false;
+							this->changeField = field::ogre;
+						}
+						break;
+					}
+				case field::shotgun:
+					{
+						this->arrayChar[this->characActive].hasShotgun = true;
+						this->changeField = field::shotgun;
+						break;
+					}
+					///=================
+					case field::swamp:
+					{
+						if (!this->arrayChar[this->characActive].hasRope)
+						{
+							this->arrayChar[this->characActive].isLive = false;
+						}
+						else
+						{
+							this->arrayChar[this->characActive].hasRope = false;
+							this->changeField = field::ogre;
+						}
+						break;
+					}
+				case field::rope:
+					{
+						this->arrayChar[this->characActive].hasRope = true;
+						this->changeField = field::rope;
+						break;
+					}
+					//============
+				case field::eagle:
+					{
+						this->coins=coins+1;
+						this->changeField = field::eagle;
+						break;
+					}
+
+				case field::coin1:
+					{
+						this->coins=coins+1;
+						this->changeField = field::coin1;
+						break;
+					}
+				case field::coin2:
+					{
+						this->coins=coins+2;
+						this->changeField = field::coin2;
+						break;
+					}
+
+					case field::coin3:
+					{
+						this->coins=coins+3;
+						this->changeField = field::coin3;
+						break;
+					}
+
+					case field::coin4:
+					{
+						this->coins=coins+4;
+						this->changeField = field::coin4;
+						break;
+					}
+
+					case field::coin5:
+					{
+						this->coins=coins+5;
+						this->changeField = field::coin5;
+						break;
+					}
+
+
+				}//switch (fieldPlace_ )
+					
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool Tplayer::getCharacterLive(int n_)
+	{
+		return this->arrayChar[n_].isLive;
+	}
+
+	int Tplayer::getCharacActive()
+	{
+		return this->characActive;
+	}
+
+	void Tplayer::doActive(int i_, int j_)
+	{
+		for (int i=0; i<3; i++)
+			if (this->arrayChar[i].i == i_ && this->arrayChar[i].j == j_ && this->arrayChar[i].isLive)
+			{
+				this->characActive = i;
+				break;
+			}
+	}
+
+	bool Tplayer::isCharacActive()
+	{
+		if (this->characActive != -1)
+		return true;
+		return false;
+	}
+
+
+	Tplayer::Tplayer(int iShip_, int jShip_)
+	{
+		iShip = iShip_;
+		jShip = jShip_;
+
+		for (int i=0; i<3; i++)
+		{
+			arrayChar[i].i = iShip;
+			arrayChar[i].j = jShip;
+			arrayChar[i].coins =0;
+			arrayChar[i].hasCar = false;
+			arrayChar[i].hasParachute = false;
+			arrayChar[i].hasRope = false;
+			arrayChar[i].hasShotgun = false;
+			arrayChar[i].inPit = false;
+			arrayChar[i].inSwamp = false;
+			arrayChar[i].isLive = true;
+			arrayChar[i].onShip = true;
+		}
+		characActive = -1;
+		coins =0;
+		
+	}
+
+
+	void player::Tplayer::killTcharacter()
+	{
+		this->arrayChar[this->characActive].isLive = false;
+		this->arrayChar[this->characActive].i = -10;
+		this->arrayChar[this->characActive].j = -10;
+		
+	}
+
+
+	void player::Tplayer::killTcharacter(int n_)
+	{
+		this->arrayChar[n_].isLive = false;
+		this->arrayChar[n_].i = -10;
+		this->arrayChar[n_].j = -10;
+		
+	}
+
+
+	int player::Tplayer::getChangeField()
+	{
+		return this->changeField;
+	}
+
+	void player::Tplayer::setChangeField(int chF_)
+	{
+		this->changeField = chF_;
+	}
+
+	bool player::Tplayer::killEnemy(player::Tcharacter enemy)
+	{
+		int radius = 1;
+		if (this->arrayChar[this->characActive].hasShotgun)
+			radius = 3;
+
+		if (myAbs(this->arrayChar[this->characActive].i - enemy.i)<=radius &&
+			myAbs(this->arrayChar[this->characActive].j - enemy.j)<=radius )
+		{
+			this->arrayChar[this->characActive].coins +=enemy.coins;
+			this->arrayChar[this->characActive].hasShotgun = false;
+			return true;
+		}
+
+		return false;
+	}
+
+	player::Tcharacter player::Tplayer::getCharacter(int n_)
+	{
+		return this->arrayChar[n_];
+	}
+
+	int player::Tplayer::getCoins()
+	{
+		return this->coins;
+	}
+
+	bool player::Tplayer::characterHasShotgun(int n_)
+	{
+		return (this->arrayChar[this->characActive].hasShotgun);
+	}
+
+	bool player::Tplayer::characterHasRope(int n_)
+	{
+		return (this->arrayChar[this->characActive].hasRope);
+	}
+
+
+}//namespace
